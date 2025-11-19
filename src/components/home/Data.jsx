5a2f13ac76de09964a83467fe8f833781b45dd0b
chamithkavinda <chamith13kavinda@gmail.com>
2025-11-19 22:34:50 +0530

    home section redesign

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -8,7 +8,7 @@ const Data = () => {
         <span className="verify-icon"></span>
       </h1>
       <h3 className="home__title">Full Stack Developer</h3>
-      <p className="home__description">Transforming lines of code into lifetime experiences</p>
+      <p className="home__description">Transforming lines of code into lifetime experiences . . . <br /> <br />Associate Software Engineer with a strong foundation in full-stack development, currently contributing to ERP systems and enterprise-level web solutions at Bio Foods Pvt Ltd. Proficient in Java, Spring Boot, React, Angular, MySQL, and MERN technologies, with a focus on efficient architecture, responsive UI/UX, and delivering impactful user experiences. I am passionate about building scalable applications, optimizing system performance, and applying clean coding principles to solve real-world business challenges. I aim to create solutions that are not only functional but also intuitive and user-friendly.</p>
     </div>
   );
 };
